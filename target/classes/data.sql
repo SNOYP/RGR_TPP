6a0832aaa59c4917207bbb9e33ab1565fc39227e
SNOYP <prymachenkovadym006@gmail.com>
2025-11-28 21:47:52 +0200

    Add gitignore

--- target/classes/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO users (username, password, role)
-VALUES ('admin', 'YWRtaW4=', 'ROLE_ADMIN')
-ON CONFLICT (username) DO NOTHING;
-
-INSERT INTO users (username, password, role)
-VALUES ('user', 'dXNlcg==', 'ROLE_USER')
-ON CONFLICT (username) DO NOTHING;
